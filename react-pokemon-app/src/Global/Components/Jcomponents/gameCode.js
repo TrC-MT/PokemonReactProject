c2d628fe1817a2d54b366a25d8cedea4c39aff00
Jacob Davies <jacobdavies@Jacobs-MacBook-Pro.local>
2022-11-29 09:24:01 -0700

    done

--- react-pokemon-app/src/Global/Components/Jcomponents/gameCode.js
+++ react-pokemon-app/src/Global/Components/Jcomponents/gameCode.js
@@ -18,14 +18,23 @@ function shuffleArray(array) {
 }
 
 export default function Jcode(){ //Remember to rename your section here
+  const fireArray = [4,5,6,37,38,58,59,77,78,126,136,146]
+  const waterArray = [7,8,9,54,55,60,61,62,72,73,79,80,86,87,90,91,98,99,116,117,118,119,120,121,129,130,131,134,138,139,140,141]
+  const grassArray = [1,2,3,43,44,45,46,47,69,70,71,102,103,114]
+  const psycicArray = [63,64,65,79,80,96,102,103,122,124,150,151]
+  const flyingArray = [6,12,16,17,18,21,22,41,42,83,84,85,123,130,142,144,145,146,149]
+  const normalArray = [16,17,18,19,20,21,22,39,40,52,53,83,84,85,108,113,115,128,132,133,137,143]
+  const poisonArray = [1,2,3,13,14,15,23,24,29,30,31,32,33,34,41,42,43,44,45,48,49,69,70,71,72,73,88,89,92,93,94,109,110]
+  const groundArray = [27,28,31,34,50,51,74,75,76,95,104,105,111,112] 
+  const bugArray = [10,11,12,13,14,13,46,47,48,49,123,127] 
+  const fairyArray = [35,36,38,39,40,122]
+  const fightingArray = [56,57,66,67,68,106,107]
+  const electricArray = [25,26,74,75,76,81,82,100,101,125,135,145 ] 
+  // const {type_selected} = useContext(AppContext_TypeSelected)
+  // const {isCardDisplaying} = useContext(AppContext_CardDisplaying)
 
-  const testArray = Array.from({length: 151}, (_, i) => i + 1)
-  console.log(testArray)
-  const {type_selected} = useContext(AppContext_TypeSelected)
-  const {isCardDisplaying} = useContext(AppContext_CardDisplaying)
 
-
-    const [displayArray, setDisplayArray] = useState([]);
+    let [displayArray, setDisplayArray] = useState([]);
     const [firstCardSelectedPosition, setFirstCardSelectedPosition ] = useState(null);
     const [secondCardSelectedPosition, setSecondCardSelectedPosition] = useState(null);
 
@@ -65,26 +74,65 @@ export default function Jcode(){ //Remember to rename your section here
       }
 
     }, [firstCardSelectedPosition, secondCardSelectedPosition, displayArray]);
-   
-
+    let randomArray = []
+    // let finalArray;
+    // if(type_selected === 'water'){
+    //   displayArray = waterArray
+    // }
+    // else if(type_selected === 'grass'){
+    //   finalArray = grassArray
+    // }
+    // else if(type_selected === 'ground'){
+    //   finalArray = groundArray
+    // }
+    // else if(type_selected === 'flying'){
+    //   finalArray = flyingArray
+    // }
+    // else if(type_selected === 'poison'){
+    //   finalArray = poisonArray
+    // }
+    // else if(type_selected === 'fire'){
+    //   finalArray = fireArray
+    // }
+    // else if(type_selected === 'normal'){
+    //   finalArray = normalArray
+    // }
+    // else if(type_selected === 'bug'){
+    //   finalArray = bugArray
+    // }
+    // else if(type_selected === 'electric'){
+    //   finalArray = electricArray
+    // }
+    // else if(type_selected === 'fighting'){
+    //   finalArray = fightingArray
+    // }
+    // else if(type_selected === 'fairy'){
+    //   finalArray = fairyArray
+    // }
+    // else if(type_selected === 'psycic'){
+    //   finalArray = psycicArray
+    // }
+    // else{
+    //   finalArray = displayArray
+    // }
     useEffect(() => {
       let displayArray;
       const defaultArray = Array.from({length: 151}, (_, i) => i + 1)
-      let randomArray = []
+      
           for(let j = 0; j < 151; j++){
             const randomNumber = Math.floor(Math.random() * 151)
             randomArray.push(defaultArray[randomNumber])
           }
-      const arrayWithUniqueTypes24 = [...new Set(randomArray)].slice(0, 151);
-      const arrayWithUniqueTypes12 = [...new Set(randomArray)].slice(0, 6)
+      const arrayWithUniqueTypes24 = [...new Set(randomArray)].slice(0, 12);
+      // const arrayWithUniqueTypes12 = [...new Set(randomArray)].slice(0, 6)
       const displayArrayWithoutPosition24 = [
         ...arrayWithUniqueTypes24,
         ...arrayWithUniqueTypes24
       ];
-      const displayArrayWithoutPosition12 = [
-        ...arrayWithUniqueTypes12,
-        ...arrayWithUniqueTypes12
-      ]
+      // const displayArrayWithoutPosition12 = [
+      //   ...arrayWithUniqueTypes12,
+      //   ...arrayWithUniqueTypes12
+      // ]
 
       // shuffle
       shuffleArray(displayArrayWithoutPosition24); //change to 24 for full size game
@@ -108,21 +156,20 @@ export default function Jcode(){ //Remember to rename your section here
         setSecondCardSelectedPosition(pos);
       }
     };
+   
     
     return(
-
             <div className='mySection' id="myTableSection">
                 <div className='container'>
-                      {displayArray.map(({ index, isHidden, position }) => !isHidden ? (
+                  {displayArray.map(({ index, isHidden, position }) => !isHidden? (
                         <>
-                          <CreateCard
+                           <CreateCard
                             onClick={() => onClick(position)}
                             key={position}
                             pokemonIndex={index + 1}
-                            typeToDisplay={type_selected}
+                            typeToDisplay={null}
                             selected={position === firstCardSelectedPosition || position === secondCardSelectedPosition} 
-                          />
-                          {/* {console.log('Index: ' + index + ' IsCardDisplaying: ' + isCardDisplaying)} */}
+                          /> 
                         </> 
                    ) : null )}
                  
@@ -135,20 +182,12 @@ export default function Jcode(){ //Remember to rename your section here
             </div>
 
             
+  
         
     ) 
-    const psycicArray = []
-    function makeCardArray(){
-      psycicArray.push( isCardDisplaying => {
-      <CreateCard
-      key={null}
-      pokemonIndex={null}
-      typeToDisplay={type_selected}
-    />
-  }
-      )
+      
     }
   
   
-        }
+        
       

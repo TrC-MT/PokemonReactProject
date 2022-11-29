d8762d01a902964681807aeecdf60d068c7edc1c
Travis Cook <travis.cook5583@stu.mtec.edu>
2022-11-29 10:25:43 -0700

    Pretty sure it's merge errors.

--- react-pokemon-app/src/Global/Components/Jcomponents/player2score.js
+++ react-pokemon-app/src/Global/Components/Jcomponents/player2score.js
@@ -2,7 +2,7 @@ import { useContext } from 'react';
 import { AppContext_PlayersNames } from '../../../AppContext';
 
 export default function Player2Score(props){
-    const [player2name] = useContext(AppContext_PlayersNames)
+    // const [player2name] = useContext(AppContext_PlayersNames)
     return(
         <div className="player2Scores">
             {props.player2name}: 2 Matches
--- react-pokemon-app/src/Global/Components/Jcomponents/player3score.js
+++ react-pokemon-app/src/Global/Components/Jcomponents/player3score.js
@@ -2,7 +2,7 @@ import { useContext } from 'react';
 import { AppContext_PlayersNames } from '../../../AppContext';
 
 export default function Player3Score(props){
-    const [player3name] = useContext(AppContext_PlayersNames)
+    // const [player3name] = useContext(AppContext_PlayersNames)
     return(
         <div className="player3Scores">
             {props.player3name}: 2 Matches

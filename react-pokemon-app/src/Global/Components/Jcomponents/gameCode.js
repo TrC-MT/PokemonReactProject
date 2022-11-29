8be93a84c5c404e82a5b365cfc32e34bf7b38f2b
Travis Cook <travis.cook5583@stu.mtec.edu>
2022-11-29 10:26:14 -0700

    Commented out typeArrays

--- react-pokemon-app/src/Global/Components/Jcomponents/gameCode.js
+++ react-pokemon-app/src/Global/Components/Jcomponents/gameCode.js
@@ -19,18 +19,18 @@ function shuffleArray(array) {
 
 export default function Jcode(){ //Remember to rename your section here
   const [players_count] = useContext(AppContext_AmountPlayers)
-  const fireArray = [4,5,6,37,38,58,59,77,78,126,136,146]
-  const waterArray = [7,8,9,54,55,60,61,62,72,73,79,80,86,87,90,91,98,99,116,117,118,119,120,121,129,130,131,134,138,139,140,141]
-  const grassArray = [1,2,3,43,44,45,46,47,69,70,71,102,103,114]
-  const psycicArray = [63,64,65,79,80,96,102,103,122,124,150,151]
-  const flyingArray = [6,12,16,17,18,21,22,41,42,83,84,85,123,130,142,144,145,146,149]
-  const normalArray = [16,17,18,19,20,21,22,39,40,52,53,83,84,85,108,113,115,128,132,133,137,143]
-  const poisonArray = [1,2,3,13,14,15,23,24,29,30,31,32,33,34,41,42,43,44,45,48,49,69,70,71,72,73,88,89,92,93,94,109,110]
-  const groundArray = [27,28,31,34,50,51,74,75,76,95,104,105,111,112] 
-  const bugArray = [10,11,12,13,14,13,46,47,48,49,123,127] 
-  const fairyArray = [35,36,38,39,40,122]
-  const fightingArray = [56,57,66,67,68,106,107]
-  const electricArray = [25,26,74,75,76,81,82,100,101,125,135,145 ] 
+  // const fireArray = [4,5,6,37,38,58,59,77,78,126,136,146]
+  // const waterArray = [7,8,9,54,55,60,61,62,72,73,79,80,86,87,90,91,98,99,116,117,118,119,120,121,129,130,131,134,138,139,140,141]
+  // const grassArray = [1,2,3,43,44,45,46,47,69,70,71,102,103,114]
+  // const psycicArray = [63,64,65,79,80,96,102,103,122,124,150,151]
+  // const flyingArray = [6,12,16,17,18,21,22,41,42,83,84,85,123,130,142,144,145,146,149]
+  // const normalArray = [16,17,18,19,20,21,22,39,40,52,53,83,84,85,108,113,115,128,132,133,137,143]
+  // const poisonArray = [1,2,3,13,14,15,23,24,29,30,31,32,33,34,41,42,43,44,45,48,49,69,70,71,72,73,88,89,92,93,94,109,110]
+  // const groundArray = [27,28,31,34,50,51,74,75,76,95,104,105,111,112] 
+  // const bugArray = [10,11,12,13,14,13,46,47,48,49,123,127] 
+  // const fairyArray = [35,36,38,39,40,122]
+  // const fightingArray = [56,57,66,67,68,106,107]
+  // const electricArray = [25,26,74,75,76,81,82,100,101,125,135,145 ] 
   // const {type_selected} = useContext(AppContext_TypeSelected)
   // const {isCardDisplaying} = useContext(AppContext_CardDisplaying)
 

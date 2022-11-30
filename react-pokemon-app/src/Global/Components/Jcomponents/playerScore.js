1dbc7c16845f9f060889b28c32fdda3a420e469f
Travis Cook <travis.cook5583@stu.mtec.edu>
2022-11-30 10:19:05 -0700

    Plural matches message.

--- react-pokemon-app/src/Global/Components/Jcomponents/playerScore.js
+++ react-pokemon-app/src/Global/Components/Jcomponents/playerScore.js
@@ -2,10 +2,23 @@ import { useContext } from 'react';
 import { AppContext_PlayersNames } from '../../../AppContext';
 
 export default function PlayerScore({player}){
-    var score = '2';
+    var score = 2;
+    var isPlural = ''
+    if(score > 1){
+        isPlural = 'Matches'
+    }
+    else if(score <= 0){
+        isPlural = ''
+    }
+    else if(score == 1){
+        isPlural = 'Match'
+    }
+    else{
+        isPlural = 'Not A Number.'
+    }
     return(
         <div className={`player${player.id}Scores`}>
-            {player.name}: {score} Matches
+            {player.name}: {score} {isPlural}
         </div>
     )
 }

cdbcb321c5af6144422eb77e7927e776753123b2
Alex Paredes <alexparedes83@gmail.com>
2022-11-29 22:46:11 -0700

    showing pokemon on third page

--- react-pokemon-app/src/sections/Walls101/ThirdPage.js
+++ react-pokemon-app/src/sections/Walls101/ThirdPage.js
@@ -2,6 +2,7 @@ import React, {useEffect, useState} from 'react';
 import '../Walls101/Wstyles.css'
 import '../hjake07/Jstyles.css'
 import '../../Global/CardCreaters/cardstyles.css'
+import All from '../hjake07/Jsection';
 
 import DeckOption from '../../Global/Components/Wcomponents/ChosenDeck'
 
@@ -12,7 +13,8 @@ export default function DeckSelection(){
             <DeckOption></DeckOption>
             <br></br>
             <br></br>
-            <div class='maincontainer'>
+            <All></All>
+            {/* <div class='maincontainer'>
                 <div class='card' id='card'>
                     <div class='front'>
                         <p>Da Front</p>
@@ -21,15 +23,15 @@ export default function DeckSelection(){
                         <p>Da Back</p>
                     </div>
                 </div>
-            </div>
+            </div> */}
         </div>
         
     )
 }
 
-const card = document.getElementById("card");
-card.addEventListener('click',flipCard);
+// const card = document.getElementById("card");
+// card.addEventListener('click',flipCard);
 
-function flipCard() {
-    flipCard.classList.toggle("flipCard");
-}
+// function flipCard() {
+//     flipCard.classList.toggle("flipCard");
+// }
